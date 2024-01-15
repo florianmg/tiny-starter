ddf1c72f3017c0d142c5b701c85f5f2e265d3ad8
florianmg <mesagouiffes@gmail.com>
2024-01-15 18:40:28 +0100

    feat: install and configure i18n

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,24 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
+import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+
+import { GetStaticPropsContext } from 'next/types';
+import { useTranslation } from 'next-i18next';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
+  const { t } = useTranslation();
   return (
     <main>
-      <p>tiny starter home page</p>
+      <p>{t('appname')}</p>
     </main>
   );
+}
+
+export async function getStaticProps(context: GetStaticPropsContext) {
+  return {
+    props: {
+      ...(await serverSideTranslations(context.locale || 'fr', ['common'])),
+    },
+  };
 }
