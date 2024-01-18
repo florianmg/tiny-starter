d8bffbec5504dcf5c5a3cf7a14eafbe39c08a9f0
florianmg <mesagouiffes@gmail.com>
2024-01-18 18:57:45 +0100

    feat(dashboard): add server side user check

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,26 +1,64 @@
+import { GetServerSidePropsContext } from 'next/types';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
+import nookies from 'nookies';
 
-import { GetStaticPropsContext } from 'next/types';
 import { useTranslation } from 'next-i18next';
 import { useAuthStore } from '@/store/auth.store';
-import NavBar from '@/components/nav-bar';
+
+import { pages } from '@/constants/pages.constants';
+import { auth } from '@/configs/admin.firebase';
 
 export default function Dashboard() {
   const { t } = useTranslation();
   const user = useAuthStore((state) => state.user);
   return (
     <main>
-      <NavBar />
       <p>this is the dashboard</p>
       <p>{user?.email}</p>
     </main>
   );
 }
 
-export async function getStaticProps(context: GetStaticPropsContext) {
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // Get cookies
+  const cookies = nookies.get(context);
+
+  // redirect home if no token in cookies
+  if (!cookies.token) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: pages.home,
+      },
+      props: {},
+    };
+  }
+
+  // verify user token validity
+  const token = await auth.verifyIdToken(cookies.token);
+
+  // Get user uid from token
+  const { uid } = token;
+
+  // Resirect home if no user in token
+  if (!uid) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: pages.home,
+      },
+      props: {},
+    };
+  }
+
+  // the user is authenticated!
+
   return {
     props: {
-      ...(await serverSideTranslations(context.locale || 'en', ['common'])),
+      ...(await serverSideTranslations(context.locale || 'fr', [
+        'common',
+        'dashboard',
+      ])),
     },
   };
 }
